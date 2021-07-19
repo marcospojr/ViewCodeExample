21333b9bff3f4fc84d8c8622a1a923d158683f12
Marcos Jr <marcospojr13@gmail.com>
2021-07-18 22:00:04 -0300

    initial setup

--- ViewcodeExample/ViewController.swift
+++ ViewcodeExample/ViewController.swift
@@ -8,14 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override func loadView() {
+        let view = UIView(frame: UIScreen.main.bounds)
+        view.backgroundColor = .red
+        self.view = view
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-     // test
-
 
 }
 
