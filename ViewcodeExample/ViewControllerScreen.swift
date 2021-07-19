9423f4ba08340c86e247a640ae1599157d76d9d3
Marcos Jr <marcospojr13@gmail.com>
2021-07-19 09:25:33 -0300

    added button

--- ViewcodeExample/ViewControllerScreen.swift
+++ ViewcodeExample/ViewControllerScreen.swift
@@ -13,7 +13,26 @@ final class ViewControllerScreen: UIView {
     override init(frame: CGRect = .zero) {
         super.init(frame: frame)
         
-        backgroundColor = .red
+        let button = UIButton(frame: .zero)
+        button.backgroundColor = .red
+        button.setTitle("Fetch", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Adicionar View
+        addSubview(button)
+        
+        // Configurar constraints
+        button.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
+        button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15).isActive = true
+        button.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -15).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+
+        
+        // Setup adicional
+        
+        
+        
+        backgroundColor = .darkGray
     }
     
     required init?(coder: NSCoder) {
